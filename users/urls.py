c32526b4ffd144bbfd766a0df921f7e9bf5bd14b
kunasyl <kunasyl45@gmail.com>
2023-04-05 01:10:21 +0600

    added authorization

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', index, name='home'),   # Главная страница
     path('<int:id>/', index_id),
-    path('register/', RegisterUser.as_view(), name='register'),
+    path('register/', RegisterUser.as_view(), name='register'),   # Регистрация
+    path('login/', LoginUser.as_view(), name='login'),   # Авторизация
 ]
 
 handler404 = pageNotFound
--- users/views.py
+++ users/views.py
@@ -1,25 +1,38 @@
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from users.forms import RegisterUserForm
+from users.forms import RegisterUserForm, LoginUserForm
 
-
+# Главная страница
 def index(request):
-    return HttpResponse('hello')
+    return render(request, 'users/home.html')
 
 def index_id(request, id):
     return HttpResponse(id)
 
+# 404
 def pageNotFound(request, exception):
 	return HttpResponseNotFound('page does not exist')
 
+# Регистрация
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')   # перенаправление на авторизацию
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Регистрация'
+        return context
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    success_url = reverse_lazy('home')   # перенапраление на главную страницу
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Авторизация'
         return context
