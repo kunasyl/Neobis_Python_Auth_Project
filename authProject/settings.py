c31f309a29fd24542ec0140b2e0a7c2cad5a665a
kunasyl <kunasyl45@gmail.com>
2023-05-02 17:21:17 +0600

    fixed security issues

--- authProject/settings.py
+++ authProject/settings.py
@@ -132,12 +132,12 @@ LOGIN_REDIRECT_URL = '/users/'
 
 
 # Emailing settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'kunasyl45@gmail.com'
-EMAIL_HOST_USER = 'kunasyl45@gmail.com'
-EMAIL_HOST_PASSWORD = 'irpdloghdwczeyiv'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = config("EMAIL_BACKEND")
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_FROM = config("EMAIL_FROM")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
 
-PASSWORD_RESET_TIMEOUT = 14400
+PASSWORD_RESET_TIMEOUT = config("PASSWORD_RESET_TIMEOUT")
